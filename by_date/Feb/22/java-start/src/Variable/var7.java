a6940994980eac88f0895f2734ca63756b4f088d
hw <brightrain453@gmail.com>
2026-02-23 05:26:04 +0900

    daily

---
+++ by_date/Feb/22/java-start/src/Variable/var7.java
@@ -0,0 +1,22 @@
+package Variable;
+
+public class var7 {
+
+    public static void main(String[] args) {
+
+        int a = 100;
+        double b = 10.5;
+        boolean c = true;
+        char d = 'A';
+        String e = "Hello jAVA";
+
+        String z = "백원";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(z);
+    }
+}
