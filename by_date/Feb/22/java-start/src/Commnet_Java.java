1c2ed2b99a88efa23cd2c77913568cecefa097de
hw <brightrain453@gmail.com>
2026-02-22 20:55:12 +0900

    21st & 22nd commit: start atcoder & java

---
+++ by_date/Feb/22/java-start/src/Commnet_Java.java
@@ -0,0 +1,11 @@
+public class Commnet_Java {
+
+    public static void main(String[] args) {
+        System.out.println("hellw java1"); //hello java1 을 출력합니다.
+        //System.out.println("hello java2");
+
+        /*
+        System.out.println("hello java3");
+         */
+    }
+}
