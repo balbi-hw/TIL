b66ddecefab264b64238e515aa34c447b89fa9a7
hw <brightrain453@gmail.com>
2026-03-02 03:14:16 +0900

    java_prac.

---
+++ by_date/MAR/02/iterate/src/loop/while2_1.java
@@ -0,0 +1,21 @@
+package loop;
+
+public class while2_1 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 10;
+
+        sum += i;
+        System.out.println("i=" + i + " sum=" + sum);
+        i ++;
+
+        sum += i;
+        System.out.println("i=" + i + " sum=" + sum);
+        i ++;
+
+        sum += i;
+        System.out.println("i=" + i + " sum=" + sum);
+
+
+    }
+}
