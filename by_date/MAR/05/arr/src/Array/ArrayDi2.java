9e648666636c927626ab4bef651cde6fda8dd050
hw <brightrain453@gmail.com>
2026-03-05 22:21:31 +0900

    check

---
+++ by_date/MAR/05/arr/src/Array/ArrayDi2.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class ArrayDi2 {
+
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int colum = 0; colum < arr[row].length; colum++) {
+                System.out.print(arr[row][colum] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
