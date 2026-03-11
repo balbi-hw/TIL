5c65e5b903694c137abd22adeb5832e7d252521a
lhw <brightrain453@gmail.com>
2026-03-11 17:55:35 +0900

    backup

---
+++ by_date/MAR/11/new.py
@@ -0,0 +1,9 @@
+'''
+extend 메서드 원리 확인
+
+context switching 시간 -- CS ( 호출 비용, 오버 헤드 )
+
+Ollama
+
+Segment Tree
+'''
--- by_tag/java2/java/src/Class/Ex1/MovieReview.java
+++
@@ -1,6 +0,0 @@
-package Class.Ex1;
-
-public class MovieReview {
-    String title;
-    String review;
-}
