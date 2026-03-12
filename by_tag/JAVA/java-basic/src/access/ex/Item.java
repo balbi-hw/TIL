1f56ad81dc89bab0f1410cc36f51f0594ad5363d
hw <brightrain453@gmail.com>
2026-03-12 19:44:34 +0900

    TOthehome

---
+++ by_tag/JAVA/java-basic/src/access/ex/Item.java
@@ -0,0 +1,13 @@
+package access.ex;
+
+public class Item {
+    String name;
+    int price;
+    int quantity;
+
+    public Item(String name, int price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
