554839ca86b4db2ba7e956b4b37b59b028dd7cbf
lhw <brightrain453@gmail.com>
2026-03-12 17:57:49 +0900

    backup

---
+++ by_tag/JAVA/java-basic/src/Construct/ConstructMain1.java
@@ -0,0 +1,14 @@
+package Construct;
+
+public class ConstructMain1 {
+    public static void main(String[] args) {
+        MemberConstruct member1 = new MemberConstruct("user1", 16, 90);
+        MemberConstruct member2 = new MemberConstruct("user2", 18, 80);
+
+        MemberConstruct[] member = {member1, member2};
+
+        for (MemberConstruct s : member) {
+            System.out.println("이름: " + s.name + " 나이 :" + s.age + " 성적: " + s.grade);
+        }
+    }
+}
