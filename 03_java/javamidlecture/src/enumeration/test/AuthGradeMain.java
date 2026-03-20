29bb29af7524ca7670525ca337cac89c1e3b8d12
hw <brightrain453@gmail.com>
2026-03-20 19:39:06 +0900

    260320 Backup: Java_enum.

---
+++ 03_java/javamidlecture/src/enumeration/test/AuthGradeMain.java
@@ -0,0 +1,15 @@
+package enumeration.test;
+
+public class AuthGradeMain {
+
+    public static void main(String[] args) {
+        AuthGrade[] values = AuthGrade.values();
+        for (AuthGrade value : values) {
+
+        }
+    }
+
+//    private void printUserInfo(AuthGrade level, String grade) {
+//        System.out.println(level.);
+//    }
+}
