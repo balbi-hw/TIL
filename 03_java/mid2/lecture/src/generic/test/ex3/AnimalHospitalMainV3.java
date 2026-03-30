bd661f11f6aeb85331a3d87bd8e77c147f9f265b
lhw <brightrain453@gmail.com>
2026-03-30 17:43:19 +0900

    Backup

---
+++ 03_java/mid2/lecture/src/generic/test/ex3/AnimalHospitalMainV3.java
@@ -0,0 +1,28 @@
+package generic.test.ex3;
+
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class AnimalHospitalMainV3 {
+
+    public static void main(String[] args) {
+        AnimalHospitalV3<Dog> dogHospital = new AnimalHospitalV3<>();
+        AnimalHospitalV3<Cat> catHospital = new AnimalHospitalV3<>();
+//        AnimalHospitalV3<Integer> integer = new AnimalHospitalV3<Integer>(); //컴파일 에러
+
+        Dog dog = new Dog("멍멍이1", 100);
+        Cat cat = new Cat("냐옹이1", 300);
+
+        dogHospital.set(dog);
+        dogHospital.checkup();
+
+        catHospital.set(cat);
+        catHospital.checkup();
+
+//        dogHospital.set(cat); // 매개변수 체크 성공: 컴파일 에러 발생
+
+        dogHospital.set(dog);
+        Dog biggerDog = dogHospital.bigger(new Dog("멍멍이2", 200));
+        System.out.println("biggerDog = " + biggerDog);
+    }
+}
