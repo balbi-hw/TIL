93596bd43544fbf865631fa779a242e8ace46cd9
hw <brightrain453@gmail.com>
2026-04-26 12:36:57 +0900

    260426 Learned: Dev. Item Domain & Make Exception - Item Domain   - Item Repository   - Item Service - Exception   - NotEnoughStockException for Item Repository

--- 03_java/boot/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ 03_java/boot/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,8 +13,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "book")
-@Getter
-@Setter
+@Getter @Setter
 public abstract class Item {
 
     @Id @GeneratedValue
@@ -26,4 +26,18 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //== 비즈니스 로직==//
+    public void addStock(int stockQuantity) {
+        this.stockQuantity += stockQuantity;
+    }
+
+    public void removeStock(int stockQuantity) {
+        int restStock = this.stockQuantity - stockQuantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
 }
