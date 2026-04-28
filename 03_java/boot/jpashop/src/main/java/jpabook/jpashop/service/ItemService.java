c0958680794b9b0d1ffbe1a60dbc772ea232cce6
lhw <brightrain453@gmail.com>
2026-04-28 09:38:47 +0900

    260428 Learned: SpringBoot - Feature. ItemUpdate - Controller   - Add update method ( recommended / not ) on ItemController     - recommended method update item by DirtyChecking by Persistence context     - not recommeded method update item by Make new Entity   - Add update method update item by Persistence context on ItemService - View   - UpdateItemForm

--- 03_java/boot/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ 03_java/boot/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -28,4 +28,13 @@ public class ItemService {
         return itemRepository.findOne(itemId);
     }
 
+    // DB 에서 불러온 객체를 변경하기 때문에 더티체킹이 가능함
+    // 영속성 컨텍스트가 자동 변경 ( Dirty checking )
+    @Transactional
+    public void updateItem(Long id, String name, int price, int stockQuantity) {
+        Item item = itemRepository.findOne(id);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
+    }
 }
